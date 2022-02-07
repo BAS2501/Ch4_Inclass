4c2aecce89c2e6dba69460a4870c9fb759d363d7
Jesus Carmona <jcarmona@tlu.edu>
2022-02-07 10:54:44 -0600

    Comments on code

---
+++ Ch4_Inclass_code.R
@@ -0,0 +1,35 @@
+# read wine.csv file
+
+
+# run Principal Componets Analysis on all the attributes
+# DO NOT NORMALIZE the attributes
+
+
+# Display the summary of the commponents 
+
+
+#display the contribution of each attribute using the $rot 
+# display only the first 5 components
+
+
+# with the results above, answer the following questions
+# 1. How many components would you choose to reach at least 80% contribution?
+# 2. Why is PC1 much greater than that of any other component?
+
+
+# run Principal Components Analysis on all the attributes
+# make sure to NORMALIZE the attributes
+
+
+# Display the summary of the components 
+
+
+#display the contribution of each attribute using the $rot 
+# display only the first 5 components
+
+
+# with the results above, answer the following questions
+# 1. How many components would you choose to reach at least 80% contribution?
+# 2. What attributes contribute the most to PC1?
+# 3. Why it is important to normalize data?
+
